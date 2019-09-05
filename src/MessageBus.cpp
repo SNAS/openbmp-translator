50d8f62bca932b454523834381ad10dcbf67a5f9
Lumin Shi <shilumin@gmail.com>
2019-09-04 17:15:47 -0700

    msg bus copied from obmp v2

---
+++ src/MessageBus.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+#include <unistd.h>
+
+#include "MessageBus.h"
+
+using namespace std;
+
+
+
+/*************************************/
+/******* singleton message bus *******/
+/*************************************/
+MessageBus *MessageBus::singleton_instance = nullptr;
+
+MessageBus *MessageBus::get_message_bus() {
+    if (!singleton_instance) {
+        cout << "initialize message bus before calling get_message_bus()." << endl;
+        exit(1);
+    }
+    return singleton_instance;
+};
+
+MessageBus *MessageBus::init() {
+    if (!singleton_instance)   // Only allow one instance of class to be generated.
+        singleton_instance = new MessageBus();
+    return singleton_instance;
+}
+
+// private constructor
+MessageBus::MessageBus() {
+    logger = Logger::get_logger();
+    config = Config::get_config();
+    producer_config = RdKafka::Conf::create(RdKafka::Conf::CONF_GLOBAL);
+    is_connected = false;
+    producer = nullptr;
+    running = true;
+}
+
+MessageBus::~MessageBus() {
+    disconnect();
+    delete singleton_instance;
+    delete producer_config;
+    delete event_callback;
+}
+
+void MessageBus::send(std::string &topic, uint8_t *encapsulated_msg, int msg_len) {
+    while (!is_connected && running) {
+        LOG_WARN("Not connected to Kafka, attempting to reconnect");
+        connect();
+        sleep(1);
+    }
+    if (!running) {
+        return;
+    }
+    RdKafka::ErrorCode resp =
+            producer->produce(topic, RdKafka::Topic::PARTITION_UA,
+                              RdKafka::Producer::RK_MSG_COPY /* Copy payload */,
+                              encapsulated_msg, msg_len, /* Value */
+                              nullptr, 0, /* Key */
+                              0, /* Timestamp (defaults to now) */
+                              nullptr, /* Message headers, if any */
+                              nullptr);
+    if (resp != RdKafka::ERR_NO_ERROR) {
+        std::cerr << "% Produce failed: " <<
+                  RdKafka::err2str(resp) << std::endl;
+    }
+    producer->poll(0);
+}
+
+void MessageBus::connect() {
+    string errstr;
+    std::ostringstream rx_bytes, tx_bytes, sess_timeout, socket_timeout;
+    std::ostringstream q_buf_max_msgs, q_buf_max_kbytes, q_buf_max_ms,
+            msg_send_max_retry, retry_backoff_ms;
+
+    disconnect();
+
+    // pass librdkafka configs from the config file to producer producer_config
+    for (auto & it : config->librdkafka_passthrough_configs) {
+        if (producer_config->set(it.first, it.second, errstr) != RdKafka::Conf::CONF_OK) {
+            LOG_ERR("Failed to configure kafka producer.");
+            throw "ERROR: Failed to configure kafka producer";
+        }
+    }
+
+    // Register event callback
+    if (event_callback == nullptr) {
+        event_callback = new KafkaEventCallback(&is_connected, logger);
+    }
+    if (producer_config->set("event_cb", event_callback, errstr) != RdKafka::Conf::CONF_OK) {
+        LOG_ERR("Failed to configure kafka event callback: %s", errstr.c_str());
+        throw "ERROR: Failed to configure kafka event callback";
+    }
+
+    // Create producer
+    producer = RdKafka::Producer::create(producer_config, errstr);
+    if (producer == nullptr) {
+        LOG_ERR("Failed to create producer: %s", errstr.c_str());
+        throw "ERROR: Failed to create producer";
+    }
+
+    is_connected = true;
+
+    // check if the connection is up
+    producer->poll(1000);
+
+    if (not is_connected) {
+        LOG_ERR("Failed to connect to Kafka, will try again in a few");
+        return;
+    }
+}
+
+void MessageBus::disconnect() {
+    if (is_connected) {
+        int i = 0;
+        while (producer->outq_len() > 0 and i < 8) {
+            LOG_INFO("Waiting for producer to finish before disconnecting: outq=%d", producer->outq_len());
+            producer->poll(500);
+            i++;
+        }
+    }
+    if (producer != nullptr) {
+        delete producer;
+    }
+    producer = nullptr;
+
+    // suggested by librdkafka to free memory
+    RdKafka::wait_destroyed(500);
+
+    is_connected = false;
+}
+
+void MessageBus::stop() {
+    running = false;
+}
+
---
+++ src/MessageBus.h
@@ -0,0 +1,132 @@
+#ifndef OPENBMP_MESSAGEBUS_H
+#define OPENBMP_MESSAGEBUS_H
+
+#include <librdkafka/rdkafkacpp.h>
+#include <librdkafka/rdkafka.h>
+#include "Logger.h"
+#include "Config.h"
+
+
+// define kafka event call back
+class KafkaEventCallback : public RdKafka::EventCb {
+public:
+    /**
+     * Constructor for callback
+     *
+     * \param isConnected[in,out]   Pointer to isConnected bool to indicate if connected or not
+     * \param logPtr[in]            Pointer to the Logger class to use for logging
+     */
+    KafkaEventCallback(bool *isConnectedRef, Logger *logPtr) {
+        isConnected = isConnectedRef;
+        logger = logPtr;
+    }
+
+    void event_cb (RdKafka::Event &event) {
+        switch (event.type())
+        {
+            case RdKafka::Event::EVENT_ERROR:
+                LOG_ERR("Kafka error: %s", RdKafka::err2str(event.err()).c_str());
+
+                if (event.err() == RdKafka::ERR__ALL_BROKERS_DOWN) {
+                    LOG_ERR("Kafka all brokers down: %s", RdKafka::err2str(event.err()).c_str());
+                    *isConnected = false;
+                }
+                if (event.err() == RdKafka::ERR__TRANSPORT) {
+                    LOG_ERR("Kafka transport error: %s", RdKafka::err2str(event.err()).c_str());
+                    *isConnected = false;
+                }
+                break;
+
+            case RdKafka::Event::EVENT_STATS:
+                LOG_INFO("Kafka stats: %s", event.str().c_str());
+                break;
+
+            case RdKafka::Event::EVENT_LOG: {
+                switch (event.severity()) {
+                    case RdKafka::Event::EVENT_SEVERITY_EMERG:
+                    case RdKafka::Event::EVENT_SEVERITY_ALERT:
+                    case RdKafka::Event::EVENT_SEVERITY_CRITICAL:
+                    case RdKafka::Event::EVENT_SEVERITY_ERROR:
+                        // rdkafka will reconnect, so no need to change at this phase
+                        LOG_ERR("Kafka LOG-%i-%s: %s", event.severity(), event.fac().c_str(),
+                                event.str().c_str());
+                        break;
+                    case RdKafka::Event::EVENT_SEVERITY_WARNING:
+                        LOG_WARN("Kafka LOG-%i-%s: %s", event.severity(), event.fac().c_str(),
+                                 event.str().c_str());
+                        break;
+                    case RdKafka::Event::EVENT_SEVERITY_NOTICE:
+                        LOG_NOTICE("Kafka LOG-%i-%s: %s", event.severity(), event.fac().c_str(),
+                                   event.str().c_str());
+                        break;
+                    default:
+                        LOG_INFO("Kafka LOG-%i-%s: %s", event.severity(), event.fac().c_str(),
+                                 event.str().c_str());
+                        break;
+                }
+                break;
+            }
+            default:
+                LOG_INFO("Kafka event type = %d (%s) %s", event.type(), RdKafka::err2str(event.err()).c_str(),
+                         event.str().c_str());
+                break;
+        }
+    }
+
+private:
+    Logger *logger;
+    bool   *isConnected;           // Indicates if connected to the broker or not.
+};
+
+class MessageBus {
+public:
+    /*********************************************************************//*
+     * Singleton class:
+     * librdkafka is thread safe,
+     * we only need one global instance
+     ***********************************************************************/
+    // initialize singleton MessageBus
+    static MessageBus *init();
+
+    // get msg bus
+    static MessageBus *get_message_bus();
+
+    // delete methods that cause problems to the singleton
+    MessageBus(MessageBus const &) = delete;
+
+    void operator=(MessageBus const &) = delete;
+
+    // destructor
+    ~MessageBus();
+
+    // send openbmp msg to kafka
+    void send(std::string &topic, uint8_t *encapsulated_msg, int msg_len);
+
+    void connect();
+
+    void disconnect();
+
+    void stop();
+
+private:
+    Logger *logger;
+    Config *config;
+
+    // private constructor for singleton design
+    explicit MessageBus();
+
+    static MessageBus *singleton_instance;
+
+    // indicates if it has connected to Kafka server
+    bool is_connected;
+    bool running;
+
+    // RdKafka variables
+    KafkaEventCallback* event_callback = nullptr;
+    RdKafka::Conf *producer_config;
+    RdKafka::Producer *producer; // Kafka Producer instance
+
+};
+
+
+#endif //OPENBMP_MESSAGEBUS_H
